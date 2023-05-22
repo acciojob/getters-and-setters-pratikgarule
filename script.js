333a2801dea254076eb564c4176a69f1d681791a
pratikgarule <127820064+pratikgarule@users.noreply.github.com>
2023-05-22 12:55:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
+	get name(){
+		return this.name;
+	}
+	set name(name){
+		this.name = name;
+	}
 
-class Student extends Person {}
+	get age(){
+		return this.age;
+	}
+	set age(age){
+		this.age = age;	
+	}
+}
 
-class Teacher extends Person {}
+class Student extends Person {
+	study(){
+		console.log(this.name+" is studying")
+	}
+}
+
+class Teacher extends Person {
+	teach(){
+		console.log(this.name+ " is teaching")
+	}
+}
+
+pers = new Person("john", 23);
+pers.name = "nadeem";
+console.log(pers.name);
 
 // Do not change the code below this line
 window.Person = Person;
